cbcc6abfa89c083102df558dbed046958fe6d162
DH_Kim <163485924+CrazyKDH@users.noreply.github.com>
2026-05-06 20:39:55 +0900

    지그재그 숫자.cpp

--- week_01/swea.cpp
+++ week_01/swea.cpp
@@ -1,6 +1,7 @@
-// 간단한 소인수분해
-// N=2a x 3b x 5c x 7d x 11e
-
+// 지그재그 숫자
+// 1부터 N까지의 숫자에서 
+// 홀수는 더하고 짝수는 뺐을 때 
+// 최종 누적된 값을 구해보자.
 #include <iostream>
 using namespace std;
 
@@ -9,38 +10,23 @@ int main(){
     cin.tie(0);
 
     int T; // 입력 받을 테스트 케이스의 수 
-    cin >> T; 
+    cin >> T;
+    
+    int sum;
 
-    // 여기서부터 내가 생각하지 못한 코드
     int N;
+
     for(int i = 1; i <= T; i++){
         cin >> N;
-    
-        int a = 0, b = 0, c = 0, d = 0, e = 0;
-        // 소인수 분해
-         while(N % 2 ==0){
-            N = N / 2;
-            a++;
-        }
-        while(N % 3 ==0){
-            N = N / 3;
-            b++;
-        }
-        while(N % 5 ==0){
-            N = N / 5;
-            c++;
-        }
-        while(N % 7 ==0){
-            N = N / 7;
-            d++;
-        }
-        while(N % 11 ==0){
-            N = N / 11;
-            e++;
-        }
-        //
+        sum = 0; // sum의 초기화 위치를 헷갈렸네연
+        for(int j = 1; j <= N; j++){
+            if(j %2 != 0)
+                sum += j;
+            else
+                sum += -j;
+            }
         cout << "#" << i << " ";
-        cout << a << " " << b << " " << c << " " << d << " " << e << "\n";
+        cout << sum << "\n";
     }
     return 0;
 }
