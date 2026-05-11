14aaffae492ebadc9be6dfa40185df8f7c69a10e
woojin-archive <tnt4088@naver.com>
2026-05-11 23:28:23 +0900

    week01 - K번째 큰 수

---
+++ week_01/26.05.11/K번 째 큰 수.py
@@ -0,0 +1,19 @@
+import sys
+# sys.stdin=open("input.txt","rt")
+
+N,K=map(int,input().split())
+a=list(map(int,input().split()))
+res=set() # 중복 제거 set 자료구조
+
+
+for i in range(N):
+    for j in range(i+1, N):
+        for s in range(j+1, N):
+            res.add(a[i]+a[j]+a[s])
+            # set 자료구조는 append(x), add(o)
+
+res=list(res) # set은 sort(x). list화 시켜주고 sort()
+res.sort(reverse=True)
+print(res[K-1])
+
+
