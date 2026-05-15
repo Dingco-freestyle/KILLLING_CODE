6685e84b1d1dc00a02e4418689b308361a48e628
woojin-archive <tnt4088@naver.com>
2026-05-15 17:57:20 +0900

    week02 - 격자판 최대합

---
+++ week_02/26.05.15/격자판 최대합.py
@@ -0,0 +1,33 @@
+import sys
+# sys.stdin = open("input.txt", "rt")
+
+N=int(input())
+a=[list(map(int,input().split())) for _ in range(N)]
+largest=-2147000000
+
+# 행,열
+for i in range(N):
+    sum1=sum2=0
+    for j in range(N):
+        sum1+=a[i][j]
+        sum2+=a[j][i]
+    if sum1>largest:
+        largest=sum1
+    if sum2>largest:
+        largest=sum2
+
+# 대각선
+sum1=sum2=0
+for i in range(N):
+    sum1+=a[i][i]
+    sum2+=a[i][N-i-1]
+if sum1>largest:
+    largest=sum1
+if sum2>largest:
+    largest=sum2
+
+print(largest)
+
+
+
+
