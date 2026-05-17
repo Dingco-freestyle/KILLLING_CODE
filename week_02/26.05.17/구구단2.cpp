90867f1cfbd4ab18a999d71500069004e0df5bb6
DH_Kim <163485924+CrazyKDH@users.noreply.github.com>
2026-05-17 17:54:35 +0900

    구구단2_김동하.cpp

---
+++ week_02/26.05.17/구구단2.cpp
@@ -0,0 +1,26 @@
+// 구구단2
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int T;
+	cin >> T;
+
+	for (int i = 1; i <= T; i++) {
+		int A, B;
+		cin >> A >> B;
+
+		int ans = 0;
+		if ((A > 9) || (B > 9))
+			ans = -1;
+		else
+			ans = A * B;
+
+		cout << "#" << i << ' ' << ans << "\n";
+	}
+	return 0;
+}
