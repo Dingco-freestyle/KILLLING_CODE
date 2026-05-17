7e6ee77cad0225e1fa36a1f9a0886b6346f5df8d
DH_Kim <163485924+CrazyKDH@users.noreply.github.com>
2026-05-17 17:51:06 +0900

    조별과제_김동하.cpp

---
+++ week_02/26.05.17/조별과제.cpp
@@ -0,0 +1,21 @@
+// 조별과제
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int T;
+	cin >> T;
+
+	for (int i = 1; i <= T; i++) {
+		int N;
+		cin >> N;
+
+		int max = N / 3;
+
+		cout << "#" << i << ' ' << max << "\n";
+	}
+}
