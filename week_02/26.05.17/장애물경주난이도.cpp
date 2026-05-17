5b92f0c6865ff685202d81c59440c8873717cb65
DH_Kim <163485924+CrazyKDH@users.noreply.github.com>
2026-05-17 17:55:36 +0900

    장애물경주난이도_김동하.cpp

---
+++ week_02/26.05.17/장애물경주난이도.cpp
@@ -0,0 +1,40 @@
+// 장애물 경주 난이도
+
+#include <iostream>
+using namespace std;
+#include <vector>
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int T;
+	cin >> T;
+
+	for (int i = 1; i <= T; i++) {
+		int N;
+		cin >> N;
+		vector<int> V;
+
+		for (int j = 0; j < N; j++) {
+			int p;
+			cin >> p;
+			V.push_back(p);
+		}
+
+		int max_up = 0, max_down = 0;
+
+		for (int k = 1; k < N; k++) {
+			if (V[k - 1] < V[k]) {
+				if ((V[k] - V[k - 1]) > max_up)
+					max_up = V[k] - V[k - 1];
+			}
+			else if (V[k - 1] > V[k]) {
+				if ((V[k - 1] - V[k]) > max_down)
+					max_down = V[k - 1] - V[k];
+			}
+		}
+		cout << "#" << i << ' ' << max_up << ' ' << max_down << "\n";
+	}
+	return 0;
+}
