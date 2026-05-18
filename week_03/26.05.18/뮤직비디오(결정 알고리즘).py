c6e83a8cfe70a50d23f96abc9a593139d8787410
woojin-archive <tnt4088@naver.com>
2026-05-18 15:08:04 +0900

    뮤직비디오(결정 알고리즘)

---
+++ week_03/26.05.18/뮤직비디오(결정 알고리즘).py
@@ -0,0 +1,35 @@
+import sys
+
+# sys.stdin = open("input.txt", "rt")
+def Count(capacity) :
+    cnt=1
+    sum=0
+    for x in Music :
+        if sum+x>capacity : #용량 넘어가면 cnt++
+            cnt+=1
+            sum=x # 다음 DVD 용량 계산
+        else :
+            sum+=x
+
+    return cnt
+
+N,M=map(int,input().split())
+Music=list(map(int,input().split()))
+
+lt=1
+rt=sum(Music) # 최대 용량
+res=0
+
+sum=0
+while lt<=rt :
+    mid=(lt+rt)//2 # 용량 초기화 (최소 용량 찾기 위해)
+
+    if Count(mid)<=M :
+        res=mid # 일단 정답
+        rt=mid-1 # 더 좋은값(최소 용량) 찾기 위해
+    else :
+        lt=mid+1 # 용량 너무 적어서 키우기
+
+print(res)
+
+
