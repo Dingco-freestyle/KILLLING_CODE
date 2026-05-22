be1c835ce2e13ed01c60e2228e89bf7c5d1c93a8
DH_Kim <163485924+CrazyKDH@users.noreply.github.com>
2026-05-22 21:48:24 +0900

    Update 민석이의과제체크하기.cpp

--- week_03/26.05.18/민석이의과제체크하기.cpp
+++ week_03/26.05.18/민석이의과제체크하기.cpp
@@ -18,25 +18,37 @@ int main() {
 		int N; // 1~N까지 수강생 수
 		int K; // 과제 제출한 사람 수
 		cin >> N >> K;
+
+		bool flag[101] = {false};
+		
 		for (int j = 0; j < K; j++) {
 			int number;
 			cin >> number;
-			v1.push_back(number);
+			flag[number] = true;
+			// v1.push_back(number);
 		}
-
-		for (int j = 1; j <= N; j++) {
+			/* 
+			내 풀이
 			if (find(v1.begin(), v1.end(), j) != v1.end())
 				continue;
-
 			else 
 				v2.push_back(j);
+			
+		}
+		sort(v2.begin(), v2.end());
+		*/
+
+		// 효율적인 풀이
+		for (int j = 1; j <= N; j++) {
+			if(!flag[j])
+				cout << j;
+		cout << "#" << i << ' ';
 		}
 
-		sort(v2.begin(), v2.end());
-		cout << "#" << i << ' ';
-
+		/*	
 		for (int a : v2) 
 			cout << a << ' ';
+		*/	
 		
 		cout << "\n";
 	}
