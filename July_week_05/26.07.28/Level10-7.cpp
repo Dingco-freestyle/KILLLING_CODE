d401a9f77a335fa995edf0f5d9eaff184a4e6eeb
DH_Kim <163485924+CrazyKDH@users.noreply.github.com>
2026-07-28 14:38:09 +0900

    Level 10-7.cpp

---
+++ July_week_05/26.07.28/Level10-7.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int GOP() {
+	int a, b;
+	cin >> a >> b;
+
+
+}
+
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int a = GOP();
+	int b = SUM();
+
+	return 0;
+}
