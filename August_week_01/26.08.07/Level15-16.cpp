6df006cb7b8e531228cef6a915dc726cd25a61be
DH_Kim <163485924+CrazyKDH@users.noreply.github.com>
2026-08-07 16:53:32 +0900

    Level 15-16.cpp (레벨 15 끄읕-)

---
+++ August_week_01/26.08.07/Level15-16.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	char arr[3][3] = { 0 };
+
+	char c;
+	cin >> c;
+
+	for (int i = 2; i >= 0; i--) {
+		for (int j = 0; j < 3 - i; j++) {
+			arr[i][j] = c;
+			c++;
+		}
+	}
+
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			if (arr[i][j] == '\0') {
+				cout << "";
+			}
+			else {
+				cout << arr[i][j];
+			}
+		}
+		cout << "\n";
+	}
+
+
+	return 0;
+}
