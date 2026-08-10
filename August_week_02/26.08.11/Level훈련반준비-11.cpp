5ac584064bded297e8ec38530fbc5bc751a6c23d
DH_Kim <163485924+CrazyKDH@users.noreply.github.com>
2026-08-11 00:46:30 +0900

    Update Level훈련반준비-11.cpp

--- August_week_02/26.08.11/Level훈련반준비-11.cpp
+++ August_week_02/26.08.11/Level훈련반준비-11.cpp
@@ -18,6 +18,18 @@ int main() {
 		v.push_back(c);
 	}
 
+	for (int i = 0; i < 6; i++) {
+		for (int j = 0; j < 5; j++) {
+			if (v[j] == arr[i]){
+				cnt += 1;
+				break;
+			}
+		}
+	}
+
+
+	
+	/*
 	for (int i = 0; i < 5; i++) {
 		for (int j = 0; j < 6; j++) {
 			if (v[i] == arr[j]){
@@ -26,7 +38,12 @@ int main() {
 			}
 		}
 	}
+	*/
 
+	// for문을 위에처럼 주석의 코드처럼 돌리면 카운트가 하나 덜 나오게 된다.
+	// 원리는 같은데 왜 그랬는지 고민해봤던 코드
+
+	
 	cout << cnt << "개 맞추었습니다";
 
 	return 0;
