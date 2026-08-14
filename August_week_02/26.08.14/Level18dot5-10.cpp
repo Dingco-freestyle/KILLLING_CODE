aaa6d68ad0f13754cf64cec8fa1c74df4788b1ff
DH_Kim <163485924+CrazyKDH@users.noreply.github.com>
2026-08-14 17:58:55 +0900

    Level 18.5-10.cpp

---
+++ August_week_02/26.08.14/Level18dot5-10.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+#include <string>
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	string str;
+	cin >> str;
+
+	int cnt[26] = { 0 };
+
+	for (int i = 0; i < str.length(); i++) {
+		cnt[str[i] - 'A']++;
+	}
+
+	for (int i = 0; i < 26; i++) {
+		if (cnt[i] > 0) {
+			cout << (char)('A' + i) << ":" << cnt[i] << "\n";
+		}
+	}
+
+	return 0;
+}
