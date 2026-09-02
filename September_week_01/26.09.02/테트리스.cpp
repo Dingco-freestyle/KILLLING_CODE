6b5363fe438c22038a7e73a514aa5f4d645b2b6f
DH_Kim <163485924+CrazyKDH@users.noreply.github.com>
2026-09-02 11:24:44 +0900

    테트리스.cpp

---
+++ September_week_01/26.09.02/테트리스.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int arr[5][4];
+	int result[5][4] = { 0 };
+
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 4; j++) {
+			cin >> arr[i][j];
+		}
+	}
+
+	int row = 4;
+
+	for (int i = 4; i >= 0; i--) {
+
+		bool flag = true;
+
+		// 하나라도 0이면 탈출
+		for (int j = 0; j < 4; j++) {
+			if (arr[i][j] == 0) {
+				flag = false;
+				break;
+			}
+		}
+
+		if (flag)
+			continue;
+
+		for (int k = 0; k < 4; k++) 
+			result[row][k] = arr[i][k];
+		
+		row--;
+	}
+
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 4; j++) {
+			cout << result[i][j] << " ";
+		}
+		cout << '\n';
+	}
+
+	return 0;
+}
